e082766319e426f54eba30ea30ad529d8ea78ff0
avinashgandham <47798481+avinashgandham@users.noreply.github.com>
2019-02-19 18:19:54 -0500

    Add files via upload

---
+++ Part01.py
@@ -0,0 +1,174 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+a=99999999999999
+a
+
+
+# In[5]:
+
+#Changes
+
+
+b=771
+b
+
+
+# In[12]:
+
+
+c=771
+c
+
+
+# In[13]:
+
+
+a=10
+b=3
+a/b
+
+
+# In[14]:
+
+
+'abc'+"ABC"
+
+
+# In[15]:
+
+
+'abc'+'def'
+
+
+# In[16]:
+
+
+'python'*3
+
+
+# In[17]:
+
+
+a="Hello"
+len(a)
+
+
+# In[18]:
+
+
+a[0]
+
+
+# In[19]:
+
+
+a[1]
+
+
+# In[20]:
+
+
+a[3]
+
+
+# In[21]:
+
+
+a[0:4]
+
+
+# In[22]:
+
+
+a[0:]
+
+
+# In[23]:
+
+
+a[3:]
+
+
+# In[24]:
+
+
+a[3:4]
+
+
+# In[25]:
+
+
+a[3:3]
+
+
+# In[26]:
+
+
+a[-1]
+
+
+# In[27]:
+
+
+a[-2]
+
+
+# In[28]:
+
+
+a[-3]
+
+
+# In[29]:
+
+
+a[-4]
+
+
+# In[30]:
+
+
+a[-5]
+
+
+# In[31]:
+
+
+'p' in "python"
+
+
+# In[41]:
+
+
+'Discover , Learning ,with ,Python'.count('a') #Assignment in part 01
+
+
+# In[44]:
+
+
+'Discover , Learning ,with ,Python'.count('o')
+
+
+# In[45]:
+
+
+'Discover , Learning ,with ,Python'.count('L')
+
+
+# In[46]:
+
+
+'Discover , Learning ,with ,Python'.count('N')
+
+
+# In[7]:
+
+
+X= "www.college.ca"
+a,b,c = X.split(".")
+print(b,c)
+
